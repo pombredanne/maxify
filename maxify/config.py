a4e30c86fa5b9cea308d1ee42800fcdb65c2a0d4
Ross Bayer <rossbayer@gmail.com>
2013-11-30 01:22:33 -0500

    Started on unit parsing from string expressions (Duration, Float, Int)

--- maxify/config.py
+++ maxify/config.py
@@ -2,12 +2,21 @@
 Module defines DSL/API for configuring a project via a Python.
 """
 
+from datetime import datetime
+from decimal import Decimal
 import inspect
+import re
 
 
-class ConfigError(Exception):
+class ConfigError(BaseException):
     """Type of error generated due to a configuration error, such as defining
     invalid units or metrics.
+    """
+    pass
+
+class DataConversionError(BaseException):
+    """Type of error generated due to a bad attempt at a data conversion.
+
     """
     pass
 
@@ -24,11 +33,11 @@ class Project(object):
         self.metrics = {}
 
     def add_metric(self,
-               name,
-               units,
-               desc=None,
-               range=None,
-               default_value=None):
+                   name,
+                   units,
+                   desc=None,
+                   range=None,
+                   default_value=None):
         if name in self.metrics:
             raise ConfigError("A metric named {0} already exists for this "
                               "project.")
@@ -64,19 +73,84 @@ class Metric(object):
 
 
 class Unit(object):
-    pass
+    @staticmethod
+    def parse(s):
+        raise NotImplementedError("Method not implemented on base class")
 
 
 class Int(Unit):
-    pass
+    @staticmethod
+    def parse(s):
+        return int(str)
 
 
 class Float(Unit):
-    pass
+    @staticmethod
+    def parse(s):
+        return Decimal(s)
 
 
 class Duration(Unit):
-    pass
+    units = (
+        ({"hours", "hour", "hrs", "hr", "h"}, 3600),
+        ({"minutes", "minute", "mins", "min", "m"}, 60),
+        ({"seconds", "second", "secs", "sec", "s"}, 1)
+    )
+
+    expr_re = re.compile("(?:(?P<unit>[A-Za-z]+)\s*(?P<num>\d+\.?\d*))|"
+                         "(?:(?P<num_alt>\d+\.?\d*)\s*(?P<unit_alt>[A-Za-z]+))")
+
+    @classmethod
+    def parse(cls, s):
+        # Examples:
+        # hrs, hours, h, hr
+        # min, mins, m, minutes
+        # sec, seconds, s, secs
+        # days, d, day
+
+        # HH:MM:SS
+        # 55:10:00
+        # 3 days, 2 hours, 10 mins
+        # mins 10
+        # 10h
+
+        # First, attempt to parse it as a time format
+        value = cls._try_parse_time_fmt(s)
+        if value:
+            return value
+
+        # TODO - Expand this to handle multiple nested expressions, like
+        # 4 hrs, 15 mins
+        match = cls.expr_re.search(s)
+        if match:
+            num = match.group("num") if match.group("num") \
+                else match.group("num_alt")
+            unit = match.group("unit") if match.group("unit") \
+                else match.group("unit_alt")
+
+            _, multiplier = [(u, m) for (u, m) in cls.units if unit in u][0]
+            value = Decimal(num) * multiplier
+
+        return value
+
+    @staticmethod
+    def _try_parse_time_fmt(s):
+        dt = None
+        try:
+            dt = datetime.strptime(s, "%H:%M:%S")
+        except BaseException:
+            pass
+
+        try:
+            dt = datetime.strptime(s, "%H:%M")
+        except BaseException:
+            pass
+
+        if dt:
+            t = dt.time()
+            return t.hour * 3600 + t.minute * 60 + t.second
+        else:
+            return None
 
 
 class Enum(Unit):
--- test/test_config.py
+++ test/test_config.py
@@ -40,14 +40,38 @@ def test_project_add_metric():
     p = Project(name="Test Project",
                 desc="Test Description")
     p.add_metric(name="Story Points",
-             units=Int,
-             desc="Estimated story points")
-
-    with pytest.raises(ConfigError):
-        p.add_metric(name="Story Points",
                  units=Int,
                  desc="Estimated story points")
 
     with pytest.raises(ConfigError):
+        p.add_metric(name="Story Points",
+                     units=Int,
+                     desc="Estimated story points")
+
+    with pytest.raises(ConfigError):
         p.add_metric(name="Bad metric",
-                 units=str)
+                     units=str)
+
+
+def test_duration_timefmt():
+    value = Duration.parse("10:05:01")
+    assert value == 36301
+
+    value = Duration.parse("10:05")
+    assert value == 36300
+
+
+def test_duration_hours():
+    value = Duration.parse("4 hours")
+    assert value == Decimal("14400")
+
+    value = Duration.parse("4.5 hours")
+    assert value == Decimal("16200")
+
+
+def test_duration_minutes():
+    value = Duration.parse("4 minutes")
+    assert value == Decimal("240")
+
+    value = Duration.parse("4.5 mins")
+    assert value == Decimal("270")
